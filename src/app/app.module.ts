9527f61d2302ec23c1815176fcbbb0cf1178f130
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2021-12-08 18:38:35 -0700

    hotfix for post merge issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import { PriceBannerComponent } from './price-banner/price-banner.component';
+import { PriceBannerComponent } from './components/price-banner/price-banner.component';
 import { MatCardModule} from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';

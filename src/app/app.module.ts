4df79bae74f44b340cc4369a9a292a5642751f3e
kendrick riley <kriley@gmail.com>
2021-12-08 21:39:47 -0500

    Added signup form, still have issues with price service http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import { PriceBannerComponent } from './price-banner/price-banner.component';
+import { PriceBannerComponent } from './components/price-banner/price-banner.component';
 import { MatCardModule} from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
-
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+// import {MatFormFieldModule, MatLabel, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+// import { MatOption } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PriceBannerComponent
+    PriceBannerComponent,
+    SignUpComponent,
+
   
   ],
   imports: [
@@ -26,9 +36,22 @@ import { HttpClientModule } from '@angular/common/http';
     MatListModule,
     MatCardModule,
     FlexLayoutModule,
-    HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    // MatFormFieldModule,
+    // MatLabel,
+    // MatOption
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatRadioModule,
+    MatGridListModule
   ],
-  providers: [],
+  providers: [
+    // {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/price-service.service.ts
+++ src/app/price-service.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Currency } from './model/currency';
+import { catchError, retry } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,11 @@ import { Currency } from './model/currency';
 export class PriceServiceService {
   
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+  }
+  getPrice(id: number) {
+    return this.http.get('https://localhost:8080/price');
+}
 // Needs work!!!
   // getPriceList(id: number): Observable<Currency[]>{
   //   return this.http.get('https://localhost:8080/price')

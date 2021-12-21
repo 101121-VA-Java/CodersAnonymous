eb8eb8f1d90c65e157e86bf040092c700e2e1f82
unknown <contact@kcsam.net>
2021-12-21 18:06:59 -0500

    Progress

--- src/app/components/chatroom/chatroom.component.ts
+++ src/app/components/chatroom/chatroom.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { ChatService } from 'src/app/services/chat.service';
 
 declare function connect(chatroomurl: string): void;
+declare function sendMessage(usrname : string): void;
+declare function addMessage(msg : string): void;
+declare function openChatBox(): void;
 
 @Component({
   selector: 'app-chatroom',
@@ -10,11 +14,33 @@ declare function connect(chatroomurl: string): void;
 })
 export class ChatroomComponent implements OnInit {
 
-  constructor() { }
+  chatHistory? : string[];
+  constructor(private ChatService: ChatService) { }
 
   ngOnInit(): void {
     var chatroomurl:string = environment.API_URL + '/chatroom';
+    this.getHistory();
     connect(chatroomurl);
   }
+
+  getHistory() {
+    this.ChatService.getChatHistory().subscribe((response: any) => {this.chatHistory = response;     
+      for (var val of this.chatHistory!) {  
+        addMessage(val); 
+      }
+    })
+    }
+  
+  sendMsg(){
+    sendMessage(sessionStorage.getItem('username') || "Guest");
+  }
+  openChat(){
+    openChatBox();
+  }
+  connectBtn() {
+    var chatroomurl:string = environment.API_URL + '/chatroom';
+    connect(chatroomurl);
+  }
+
 
 }
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,21 @@
 var stompClient = null;
 
+$( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      maxHeight: 400,
+      overflow:'scroll',
+        });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
+
+function openChatBox() {
+    $( "#dialog" ).dialog( "open" );
+  };
+
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
@@ -22,7 +38,7 @@ function connect(chatroomurl) {
         setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/message', function (messages) {
-            addMessage(messages.body);
+            addMessage(messages.body);            
         });
     });
 }
@@ -35,12 +51,24 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+function sendMessage(username) {
+    stompClient.send("/app/hello", {}, JSON.stringify({'message': $("#name").val(),
+    "username" : username
+
+}));
 }
 
+
 function addMessage(message) {
-    $("#messages").append("<tr><td>" + message + "</td></tr>");
+    // $("#messages").append("<tr><td>" + message + "</td></tr>");
+    $("#usrmsgs").append(`    
+    <div class="container" style = "border: 2px solid #dedede;">
+  <p>${message}</p>  
+</div>    
+    `);
+
+
+
 }
 
 $(function () {
@@ -49,6 +77,6 @@ $(function () {
     });
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    $( "#send" ).click(function() { sendMessage(); });
 });
 
